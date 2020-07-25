98b56ce12f5078dd0e3b1c16e766bc78103457d2
Shehroze Ahmed <shozi91@gmail.com>
2020-07-25 13:15:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,18 +26,12 @@ def etl():
         app.config["IMAGE_UPLOADS"] = os.path.join(os.path.dirname(os.getcwd()), "Upload", "Saved", "normal")
         extract.extractor(request.method, app.config["IMAGE_UPLOADS"])
 
-<<<<<<< HEAD:ETL/main.py
-    #extract the data
-    app.config["IMAGE_UPLOADS"] = os.path.join(os.path.dirname(os.getcwd()), "Upload", "saved", "normal")
-    extractor(request.method, app.config["IMAGE_UPLOADS"])
-=======
         #transform the data
         predictions, y_class = transform.transformer()
         #load the data
         load.loader(predictions, y_class)
     except Exception as e:
         print(e)
->>>>>>> b58782163f47253a8766cd6256ccf1d0968fa5fd:app.py
 
 
     return render_template("index.html")
